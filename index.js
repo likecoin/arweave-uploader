6468722d730a500841ca660fd84b77be2f5d235f
Wei-Jun Hung <weijunhung@outlook.com>
2021-10-28 10:39:34 +0800

    🚸 Directly replace original output file

--- index.js
+++ index.js
@@ -165,7 +165,7 @@ async function run() {
   const input = parseCSV(inputBuffer);
 
   const header = handleHeader(input[0]);
-  fs.appendFileSync(OUTPUT_FILE_NAME, stringifyCSV([header]));
+  fs.writeFileSync(OUTPUT_FILE_NAME, stringifyCSV([header]));
 
   const filenameIndex = header.indexOf('filename');
   const ipfsHashIndex = header.indexOf('ipfsHash');
