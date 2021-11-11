dd578ff471b2770ec4bbeafc52e0909d9c108eb4
William Chong <me@williamchong.cloud>
2021-11-11 22:31:36 +0800

    ♻️ Use ipfs get instead of cat

--- utils/ipfs.js
+++ utils/ipfs.js
@@ -1,12 +1,42 @@
 const { create } = require('ipfs-http-client');
+const { extract } = require('it-tar');
+const { pipe } = require('it-pipe');
+const toBuffer = require('it-to-buffer');
+const map = require('it-map');
 const all = require('it-all');
-const { concat } = require('uint8arrays/concat');
 
 const ipfs = create({ url: 'https://ipfs.infura.io:5001/api/v0' });
 
+async function* tarballed(source) {
+  yield* pipe(
+    source,
+    extract(),
+    // eslint-disable-next-line func-names
+    async function* (src) {
+      // eslint-disable-next-line no-restricted-syntax
+      for await (const entry of src) {
+        yield {
+          ...entry,
+          name: entry.header.name,
+          buffer: await toBuffer(map(entry.body, (buf) => buf.slice())),
+        };
+      }
+    },
+  );
+}
+
+async function collect(source) {
+  return all(source);
+}
+
 async function loadFileFromIPFS(ipfsHash) {
   try {
-    return concat(await all(ipfs.cat(ipfsHash)));
+    const output = await pipe(
+      ipfs.get(ipfsHash),
+      tarballed,
+      collect,
+    );
+    return output;
   } catch (error) {
     throw new Error(`Cannot get file from IPFS: ${ipfsHash}`);
   }
